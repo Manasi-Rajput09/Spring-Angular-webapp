01babee9bcc2eea79b12b8afe71814fbbb7ad5c3
Manasi Rajput <rajmanas@shernet.sheridancollege.ca>
2026-04-14 14:54:41 -0400

    CSS added

--- src/main/webapp/src/app/student/student-home/student-home.ts
+++ src/main/webapp/src/app/student/student-home/student-home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-student-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './student-home.html',
   styleUrl: './student-home.css',
 })
--- src/main/webapp/src/app/student/student.ts
+++ src/main/webapp/src/app/student/student.ts
@@ -3,11 +3,11 @@ import { StudentAdd } from './student-add/student-add';
 import { StudentList } from './student-list/student-list'
 
 //new 
-import { RouterLink, RouterOutlet, Routes } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-student',
-  imports: [StudentAdd, StudentList, RouterLink, RouterOutlet],
+  imports: [StudentAdd, StudentList, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './student.html',
   styleUrl: './student.css',
 })
